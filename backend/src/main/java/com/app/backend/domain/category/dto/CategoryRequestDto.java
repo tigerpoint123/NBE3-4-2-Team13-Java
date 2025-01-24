41a39b88e40b399b1aab39e49f6beaaa63cc2886
gaeun220 <gaeun2701@gmail.com>
2025-01-24 23:53:26 +0900

    feat : dto 생성

---
+++ backend/src/main/java/com/app/backend/domain/category/dto/CategoryRequestDto.java
@@ -0,0 +1,15 @@
+package com.app.backend.domain.category.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryRequestDto {
+
+	@NotBlank(message = "카테고리 이름은 필수입니다.")
+	private String name;
+}
--- backend/src/main/java/com/app/backend/domain/category/service/CategoryService.java
+++ backend/src/main/java/com/app/backend/domain/category/service/CategoryService.java
@@ -2,9 +2,14 @@ package com.app.backend.domain.category.service;
 
 import org.springframework.stereotype.Service;
 
+import com.app.backend.domain.category.repository.CategoryRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
+
+	private final CategoryRepository categoryRepository;
+
 }
