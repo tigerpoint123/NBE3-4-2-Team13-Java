c8281fe6c5b12cd9ba5acbf173b278956234fa4d
swjoon <jetkid1446@gmail.com>
2025-01-24 11:34:08 +0900

    feat : service 클래스 생성

---
+++ backend/src/main/java/com/app/backend/domain/post/service/PostService.java
@@ -0,0 +1,15 @@
+package com.app.backend.domain.post.service;
+
+import com.app.backend.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PostService {
+
+    private final PostRepository postRepository;
+
+}
