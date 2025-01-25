8cc7ad57c41d7f5eeb3e276c103fb9d4beb597c3
gaeun220 <gaeun2701@gmail.com>
2025-01-25 11:11:03 +0900

    feat: 카테고리 생성

--- backend/src/main/java/com/app/backend/domain/category/controller/CategoryController.java
+++ backend/src/main/java/com/app/backend/domain/category/controller/CategoryController.java
@@ -1,11 +1,22 @@
 package com.app.backend.domain.category.controller;
 
+import org.hibernate.validator.constraints.Length;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.backend.domain.category.dto.CategoryDto;
+import com.app.backend.domain.category.entity.Category;
 import com.app.backend.domain.category.service.CategoryService;
+import com.app.backend.domain.post.entity.Post;
+import com.app.backend.global.dto.response.ApiResponse;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -14,5 +25,25 @@ import lombok.RequiredArgsConstructor;
 public class CategoryController {
 
 	private final CategoryService categoryService;
+
+	public record CreateCategoryReqBody(
+		@NotBlank(message = "카테고리 이름은 필수입니다.")
+		@Length(max = 10, message = "카테고리 이름은 최대 10자까지 가능합니다.")
+		String name
+	) {
+	}
+
+	@PostMapping
+	public ApiResponse<CategoryDto> createCategory(
+		@RequestBody @Valid CreateCategoryReqBody request
+	) {
+		Category category = categoryService.create(request.name());
+
+		return ApiResponse.of(
+			true,
+			"201",
+			"%s 카테고리가 생성되었습니다.".formatted(category.getName())
+		);
+	}
 
 }
--- backend/src/main/java/com/app/backend/domain/category/service/CategoryService.java
+++ backend/src/main/java/com/app/backend/domain/category/service/CategoryService.java
@@ -1,18 +1,29 @@
 package com.app.backend.domain.category.service;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.app.backend.domain.category.dto.CategoryRequest;
 import com.app.backend.domain.category.entity.Category;
 import com.app.backend.domain.category.repository.CategoryRepository;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CategoryService {
 
 	private final CategoryRepository categoryRepository;
 
+	@Transactional
+	public Category create(String name) {
+
+		Category category = Category.builder()
+			.name(name)
+			.build();
+
+		return categoryRepository.save(category);
+	}
 }
