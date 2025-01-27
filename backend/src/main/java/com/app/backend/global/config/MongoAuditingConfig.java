7cd23a03e6426cde551df9de3cc2cee0dd12b83e
vdvhk12 <vdvhk12@gmail.com>
2025-01-27 17:58:50 +0900

    feat: MongoAuditingConfig 파일 추가

---
+++ backend/src/main/java/com/app/backend/global/config/MongoAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.app.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoAuditingConfig {
+}
