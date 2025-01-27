e57fce7efffc8c1cfdb38af8a4ac395c1e5f7576
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-01-27 15:08:13 +0900

    feat: GroupController 추가
    - API 명세서 기반 CRUD 구현
    - GroupMessageConstant 추가: 컨트롤러 응답 메시지 일괄 관리 목적
    - GroupRequest 내 Long groupId 필드 @NotNull 삭제

---
+++ backend/src/main/java/com/app/backend/domain/group/constant/GroupMessageConstant.java
@@ -0,0 +1,9 @@
+package com.app.backend.domain.group.constant;
+
+public abstract class GroupMessageConstant {
+    public static final String CREATE_GROUP_SUCCESS = "모임이 성공적으로 생성되었습니다.";
+    public static final String READ_GROUP_SUCCESS   = "모임이 성공적으로 조회되었습니다.";
+    public static final String READ_GROUPS_SUCCESS  = "모임 목록이 성공적으로 조회되었습니다.";
+    public static final String UPDATE_GROUP_SUCCESS = "모임이 성공적으로 수정되었습니다.";
+    public static final String DELETE_GROUP_SUCCESS = "모임이 성공적으로 삭제되었습니다.";
+}
