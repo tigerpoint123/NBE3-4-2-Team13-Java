fc9120e4a1aef36021caa8c7fd399873bd724bd9
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-01-27 16:28:42 +0900

    fix: 테스트 내 OAuth2 설정 추가 등
    - application-test_auth 수정
      - OAuth2 적용으로 인한 더미 설정값 추가
    - WebMvcTestSupporter 테스트 범위 수정
      - 기존: @WebMvcTest
      - 수정: @SpringBootTest

--- backend/src/test/java/com/app/backend/domain/group/supporter/WebMvcTestSupporter.java
+++ backend/src/test/java/com/app/backend/domain/group/supporter/WebMvcTestSupporter.java
@@ -1,19 +1,19 @@
 package com.app.backend.domain.group.supporter;
 
-import com.app.backend.domain.group.controller.GroupController;
 import com.app.backend.domain.group.service.GroupService;
-import com.app.backend.global.config.SecurityConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ActiveProfiles("test")
-@Import(SecurityConfig.class)
-@WebMvcTest(GroupController.class)
+//@Import(SecurityConfig.class)
+//@WebMvcTest(GroupController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public abstract class WebMvcTestSupporter {
 
     @Autowired
