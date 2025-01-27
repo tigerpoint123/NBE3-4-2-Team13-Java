819d5a76d598f866967adf03be4ee92e7d208f31
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-01-27 15:06:46 +0900

    feat: global 패키지 내 수정
    - ApiResponse 수정
      - static 메서드 2개 추가: HttpStatus 활용
      - 파라미터 일괄 수정: @NonNull -> @NotNull
    - @EnableJpaAuditing 설정 분리
      - @WebMvcTest 시 JPA Metamodel 에러 방지

--- backend/src/main/java/com/app/backend/global/dto/response/ApiResponse.java
+++ backend/src/main/java/com/app/backend/global/dto/response/ApiResponse.java
@@ -2,35 +2,49 @@ package com.app.backend.global.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @JsonInclude(Include.NON_NULL)
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiResponse<T> {
 
-    @NonNull
+    @NotNull
     private final Boolean isSuccess;
-    @NonNull
+    @NotNull
     private final String  code;
-    @NonNull
+    @NotNull
     private final String  message;
     private final T       data;
 
-    public static <T> ApiResponse<T> of(@NonNull final Boolean isSuccess,
-                                        @NonNull final String code,
-                                        @NonNull final String message) {
+    public static <T> ApiResponse<T> of(@NotNull final Boolean isSuccess,
+                                        @NotNull final String code,
+                                        @NotNull final String message) {
         return new ApiResponse<>(isSuccess, code, message, null);
     }
 
-    public static <T> ApiResponse<T> of(@NonNull final Boolean isSuccess,
-                                        @NonNull final String code,
-                                        @NonNull final String message,
-                                        @NonNull final T data) {
+    public static <T> ApiResponse<T> of(@NotNull final Boolean isSuccess,
+                                        @NotNull final HttpStatus status,
+                                        @NotNull final String message) {
+        return new ApiResponse<>(isSuccess, String.valueOf(status.value()), message, null);
+    }
+
+    public static <T> ApiResponse<T> of(@NotNull final Boolean isSuccess,
+                                        @NotNull final String code,
+                                        @NotNull final String message,
+                                        @NotNull final T data) {
         return new ApiResponse<>(isSuccess, code, message, data);
+    }
+
+    public static <T> ApiResponse<T> of(@NotNull final Boolean isSuccess,
+                                        @NotNull final HttpStatus status,
+                                        @NotNull final String message,
+                                        @NotNull final T data) {
+        return new ApiResponse<>(isSuccess, String.valueOf(status.value()), message, data);
     }
 
 }
