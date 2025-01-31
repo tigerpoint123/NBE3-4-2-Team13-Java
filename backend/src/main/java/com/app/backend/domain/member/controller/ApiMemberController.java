8c36ab2516296edd62c8ced1f28acdb052c7e63f
tigerpoint123 <tigerrla@naver.com>
2025-01-31 12:41:45 +0900

    feat : modify, modify test

--- backend/src/main/java/com/app/backend/domain/member/controller/ApiMemberController.java
+++ backend/src/main/java/com/app/backend/domain/member/controller/ApiMemberController.java
@@ -2,14 +2,12 @@ package com.app.backend.domain.member.controller;
 
 import java.util.Optional;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.backend.domain.member.dto.request.MemberJoinRequestDto;
