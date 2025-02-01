5395b0ec8f53e1133a99a811d06fcc37cd6b0853
gaeun220 <gaeun2701@gmail.com>
2025-02-01 20:04:30 +0900

    fix: fix

---
+++ backend/src/main/java/com/app/backend/domain/meetingApplication/dto/MeetingApplicationReqBody.java
@@ -0,0 +1,12 @@
+package com.app.backend.domain.meetingApplication.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record MeetingApplicationReqBody(
+	@NotNull
+	Long memberId, // TODO : memberId 입력받는게 아니라 jwt에서 memberId 받는 걸로 수정
+	@NotBlank
+	String context
+) {
+}
