58f281591327086ca0a6b5ce2aa6fe845b2d74a2
baekminjin <bhs3392@naver.com>
2025-02-03 09:51:18 +0900

    feat: 테스트 생성

---
+++ backend/src/test/java/com/app/backend/domain/comment/controller/CommentControllerTest.java
@@ -0,0 +1,126 @@
+package com.app.backend.domain.comment.controller;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.app.backend.domain.comment.repository.CommentRepository;
+import com.app.backend.domain.member.entity.Member;
+import com.app.backend.domain.member.repository.MemberRepository;
+import com.app.backend.domain.post.entity.Post;
+import com.app.backend.domain.post.entity.PostStatus;
+import com.app.backend.domain.post.repository.post.PostRepository;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
+public class CommentControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+	@Autowired
+	private PostRepository postRepository;
+	@Autowired
+	private MemberRepository memberRepository;
+
+	private Long testPostId;
+	private Member testMember;
+
+	@BeforeEach
+	void setUp() {
+		// 테스트용
+		testMember = Member.builder()
+			.username("testUser")
+			.password("password")
+			.nickname("테스터")
+			.role("USER")
+			.disabled(false)
+			.build();
+		memberRepository.save(testMember);
+
+
+		Post post = Post.builder()
+			.title("테스트 게시글")
+			.content("테스트 내용")
+			.postStatus(PostStatus.PUBLIC)
+			.groupId(1L)
+			.memberId(testMember.getId())
+			.build();
+
+		Post savedPost = postRepository.save(post);
+		testPostId = savedPost.getId();
+	}
+
+	@Test
+	@DisplayName("댓글 작성")
+	void createComment() throws Exception {
+
+		ResultActions resultActions = mvc
+			.perform(
+				post("/api/v1/comment/" + testPostId)
+					.content("""
+						{
+							"content": "test"
+						}
+						""")
+					.contentType(
+						new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
+					)
+					.with(user(testMember.getUsername()))
+			)
+			.andDo(print());
+
+		resultActions
+			.andExpect(handler().handlerType(CommentController.class))
+			.andExpect(handler().methodName("createComment"))
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("$.isSuccess").value(true))
+			.andExpect(jsonPath("$.code").value("201"))
+			.andExpect(jsonPath("$.message").exists());
+	}
+
+	@Test
+	@DisplayName("댓글 작성 실패 (내용 공백)")
+	void createComment2() throws Exception {
+
+		ResultActions resultActions = mvc
+			.perform(
+				post("/api/v1/comment/" + testPostId)
+					.content("""
+						{
+							"content": ""
+						}
+						""")
+					.contentType(
+						new MediaType(MediaType.APPLICATION_JSON)
+					)
+					.with(user(testMember.getUsername()))
+			)
+			.andDo(print());
+
+		resultActions
+			.andExpect(handler().handlerType(CommentController.class))
+			.andExpect(handler().methodName("createComment"))
+			.andExpect(jsonPath("$.isSuccess").value(false))
+			.andExpect(jsonPath("$.code").value("CM004"))
+			.andExpect(jsonPath("$.message").value("댓글 내용이 유효하지 않습니다"));
+	}
+
+
+}
