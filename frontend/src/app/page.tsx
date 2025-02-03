b332b83addee83f1f236c38170bd3b0a3c769132
vdvhk12 <vdvhk12@gmail.com>
2025-02-03 12:02:50 +0900

    feat: 메인 페이지 카카오 로그인 url 수정

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,19 +2,21 @@ import { Button } from "@/components/ui/button";
 import { MessageCircle } from "lucide-react";
 
 export default function Home() {
-  const socialLoginForKakaoUrl = `http://localhost:8080/oauth2/authorization/kakao`;
-  const redirectUrlAfterSocialLogin = "http://localhost:3000";
+  const socialLoginForKakaoUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}`;
+  const redirectUrlAfterSocialLogin = `http://localhost:8080/api/v1/members/kakao/callback`;
 
   return (
     <div className="flex-1 flex justify-center items-center">
-      <Button variant="outline" asChild>
-        <a
-          href={`${socialLoginForKakaoUrl}?redirectUrl=${redirectUrlAfterSocialLogin}`}
-        >
-          <MessageCircle />
-          <span className="font-bold">카카오 로그인</span>
-        </a>
-      </Button>
+      <div className="bg-red-500 on:bg-blue-500">
+        <Button variant="outline" asChild>
+          <a
+            href={`${socialLoginForKakaoUrl}&redirect_uri=${redirectUrlAfterSocialLogin}&response_type=code`}
+          >
+            <MessageCircle />
+            <span className="font-bold">카카오 로그인</span>
+          </a>
+        </Button>
+      </div>
     </div>
   );
 }
