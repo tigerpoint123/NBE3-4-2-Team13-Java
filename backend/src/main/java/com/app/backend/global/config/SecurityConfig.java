e1ba33752c700bc1849841f1d36b83b1b79a05b2
tigerpoint123 <tigerrla@naver.com>
2025-02-05 16:30:53 +0900

    bug : kakao login success in front. test fail

--- backend/src/main/java/com/app/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/app/backend/global/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
 				.requestMatchers("/oauth2/authorization/**", "/login/oauth2/code/*").permitAll()
 				.requestMatchers("/api/v1/members/kakao/**").permitAll()
 				.requestMatchers("/api/v1/download/**").authenticated()
+				.requestMatchers("/ws/**").permitAll()
 				.anyRequest().authenticated())
 			.headers(headers -> headers
 				.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
