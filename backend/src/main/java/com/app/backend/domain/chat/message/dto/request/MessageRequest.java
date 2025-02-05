cd9a6958c2e7e8cf250227e54c0364685216d959
vdvhk12 <vdvhk12@gmail.com>
2025-02-05 14:49:03 +0900

    feat: 웹 소켓 메세지 저장 후 채팅방으로 브로드 캐스팅
    - 예외 처리, 웹소켓 인증 필터는 기본 기능을 모두 완성하고 추가할 것

--- backend/src/main/java/com/app/backend/domain/chat/message/dto/request/MessageRequest.java
+++ backend/src/main/java/com/app/backend/domain/chat/message/dto/request/MessageRequest.java
@@ -1,4 +1,16 @@
 package com.app.backend.domain.chat.message.dto.request;
 
+import com.app.backend.domain.chat.message.entity.Message;
+
 public record MessageRequest (Long chatRoomId, Long senderId, String senderNickname, String content){
+
+	public Message toEntity() {
+		return Message.builder()
+			.chatRoomId(chatRoomId)
+			.senderId(senderId)
+			.senderNickname(senderNickname)
+			.content(content)
+			.disabled(false)
+			.build();
+	}
 }
