92ae514a5ef03458f2fcd44cc3722396d0e951e1
gaeun220 <gaeun2701@gmail.com>
2025-02-05 11:37:40 +0900

    test: 서비스 테스트 작성

--- backend/src/main/java/com/app/backend/domain/meetingApplication/repository/MeetingApplicationRepository.java
+++ backend/src/main/java/com/app/backend/domain/meetingApplication/repository/MeetingApplicationRepository.java
@@ -8,9 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.app.backend.domain.meetingApplication.entity.MeetingApplication;
 
 public interface MeetingApplicationRepository extends JpaRepository<MeetingApplication, Long> {
-	List<MeetingApplication> findByGroupId(Long id);
 
 	List<MeetingApplication> findByGroupIdAndDisabled(Long groupId, boolean disabled);
 
 	Optional<MeetingApplication> findByGroupIdAndIdAndDisabled(Long groupId, Long meetingApplicationId, boolean disabled);
+
+	Optional<MeetingApplication> findByIdAndDisabled(Long id, boolean disabled);
 }
