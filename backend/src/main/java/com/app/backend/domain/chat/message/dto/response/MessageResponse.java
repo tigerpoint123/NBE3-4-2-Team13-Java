18b7aa57a36acdd6f1615917fa29128258df5001
vdvhk12 <vdvhk12@gmail.com>
2025-02-05 14:44:28 +0900

    refactor: 메세지 응답에 createdAt 타입 String 으로 변경

--- backend/src/main/java/com/app/backend/domain/chat/message/dto/response/MessageResponse.java
+++ backend/src/main/java/com/app/backend/domain/chat/message/dto/response/MessageResponse.java
@@ -1,12 +1,11 @@
 package com.app.backend.domain.chat.message.dto.response;
 
-import java.time.LocalDateTime;
 import com.app.backend.domain.chat.message.entity.Message;
 import lombok.Builder;
 
 @Builder
 public record MessageResponse(String id, Long chatRoomId, Long senderId, String senderNickname, String content,
-							  LocalDateTime createdAt) {
+							  String createdAt) {
 
 	public static MessageResponse from(Message message) {
 		return MessageResponse.builder()
@@ -15,7 +14,7 @@ public record MessageResponse(String id, Long chatRoomId, Long senderId, String 
 			.senderId(message.getSenderId())
 			.senderNickname(message.getSenderNickname())
 			.content(message.getContent())
-			.createdAt(message.getCreatedAt())
+			.createdAt(message.getCreatedAt().toString())
 			.build();
 	}
 }
