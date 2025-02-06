1dade59f5d74be5b2849112d7f3ded55fe1fba2b
vdvhk12 <vdvhk12@gmail.com>
2025-02-06 15:43:28 +0900

    test: 채팅방 목록 조회 테스트 수정

--- backend/src/test/java/com/app/backend/domain/chat/room/service/ChatRoomServiceTest.java
+++ backend/src/test/java/com/app/backend/domain/chat/room/service/ChatRoomServiceTest.java
@@ -14,9 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.app.backend.domain.chat.room.dto.response.ChatRoomListResponse;
-import com.app.backend.domain.chat.room.entity.ChatRoom;
 import com.app.backend.domain.chat.room.repository.ChatRoomRepository;
-import com.app.backend.domain.group.entity.Group;
 
 @ExtendWith(MockitoExtension.class)
 class ChatRoomServiceTest {
@@ -33,17 +31,21 @@ class ChatRoomServiceTest {
 		//given
 		Long MemberId = 1L;
 
-		ChatRoom chatRoom1 = ChatRoom.builder()
-			.id(1L)
-			.group(Group.builder().id(1L).build())
+		ChatRoomListResponse chatRoom1 = ChatRoomListResponse.builder()
+			.chatRoomId(1L)
+			.groupId(1L)
+			.groupName("Group 1")
+			.participant(10L) // 가상의 참여 인원 수 추가
 			.build();
 
-		ChatRoom chatRoom2 = ChatRoom.builder()
-			.id(2L)
-			.group(Group.builder().id(3L).build())
+		ChatRoomListResponse chatRoom2 = ChatRoomListResponse.builder()
+			.chatRoomId(2L)
+			.groupId(3L)
+			.groupName("Group 2")
+			.participant(5L) // 가상의 참여 인원 수 추가
 			.build();
 
-		List<ChatRoom> chatRooms = List.of(chatRoom1, chatRoom2);
+		List<ChatRoomListResponse> chatRooms = List.of(chatRoom1, chatRoom2);
 		when(chatRoomRepository.findAllByMemberId(any(Long.class))).thenReturn(chatRooms);
 
 		//when
@@ -52,11 +54,13 @@ class ChatRoomServiceTest {
 		//then
 		assertThat(result).isNotNull();
 		assertThat(result.size()).isEqualTo(2);
-		assertThat(result.get(0).chatRoomId()).isEqualTo(chatRoom1.getId());
-		assertThat(result.get(0).groupId()).isEqualTo(chatRoom1.getGroup().getId());
-		assertThat(result.get(1).chatRoomId()).isEqualTo(chatRoom2.getId());
-		assertThat(result.get(1).groupId()).isEqualTo(chatRoom2.getGroup().getId());
-
-
+		assertThat(result.get(0).chatRoomId()).isEqualTo(chatRoom1.chatRoomId());
+		assertThat(result.get(0).groupId()).isEqualTo(chatRoom1.groupId());
+		assertThat(result.get(0).groupName()).isEqualTo(chatRoom1.groupName());
+		assertThat(result.get(0).participant()).isEqualTo(chatRoom1.participant());
+		assertThat(result.get(1).chatRoomId()).isEqualTo(chatRoom2.chatRoomId());
+		assertThat(result.get(1).groupId()).isEqualTo(chatRoom2.groupId());
+		assertThat(result.get(1).groupName()).isEqualTo(chatRoom2.groupName());
+		assertThat(result.get(1).participant()).isEqualTo(chatRoom2.participant());
 	}
 }
