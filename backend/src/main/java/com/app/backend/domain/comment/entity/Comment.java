8abb18c5d71889fa1df15d9f458d811a23a61025
baekminjin <bhs3392@naver.com>
2025-02-06 14:25:31 +0900

    feat: 엔티티 추가

--- backend/src/main/java/com/app/backend/domain/comment/entity/Comment.java
+++ backend/src/main/java/com/app/backend/domain/comment/entity/Comment.java
@@ -1,16 +1,21 @@
 package com.app.backend.domain.comment.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.app.backend.domain.member.entity.Member;
 import com.app.backend.domain.post.entity.Post;
 import com.app.backend.global.entity.BaseEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -42,6 +47,14 @@ public class Comment extends BaseEntity {
 	@ManyToOne
 	@JoinColumn(name = "member_id")
 	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "parent_id")
+	private Comment parent;
+
+	@Builder.Default
+	@OneToMany(mappedBy = "parent")
+	private List<Comment> children = new ArrayList<>();
 
 
 	public void delete() {
