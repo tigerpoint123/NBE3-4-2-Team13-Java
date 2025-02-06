c812ba7f604d63919fe5ac7c1586447c1f7e0a92
baekminjin <bhs3392@naver.com>
2025-02-06 14:26:08 +0900

    feat: 대댓글 에러코드 추가

--- backend/src/main/java/com/app/backend/domain/comment/exception/CommentErrorCode.java
+++ backend/src/main/java/com/app/backend/domain/comment/exception/CommentErrorCode.java
@@ -1,9 +1,11 @@
 package com.app.backend.domain.comment.exception;
 
+import org.springframework.http.HttpStatus;
+
 import com.app.backend.global.error.exception.DomainErrorCode;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
@@ -12,7 +14,9 @@ public enum CommentErrorCode implements DomainErrorCode {
 	COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "CM001", "댓글을 찾을 수 없습니다"),
 	COMMENT_CREATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "CM002", "댓글 생성에 실패했습니다"),
 	COMMENT_ACCESS_DENIED(HttpStatus.FORBIDDEN, "CM003", "댓글에 대한 권한이 없습니다"),
-	COMMENT_INVALID_CONTENT(HttpStatus.BAD_REQUEST, "CM004", "댓글 내용이 유효하지 않습니다");
+	COMMENT_INVALID_CONTENT(HttpStatus.BAD_REQUEST, "CM004", "댓글 내용이 유효하지 않습니다"),
+	PARENT_COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "CM005", "부모 댓글을 찾을 수 없습니다");
+
 
 
 
