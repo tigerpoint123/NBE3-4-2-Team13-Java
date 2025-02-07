828a9c25c445676d24c1a083bc5f30ea296f2e9e
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-02-07 14:07:36 +0900

    feat: 어노테이션 파라미터 설정 추가
    - 스프링 기본 Page 객체 응답 시 필요/불필요 데이터 선택을 위한 AOP
    - 어노테이션의 파라미터를 false 또는 true로 설정함으로써 해당 값의 포함 여부를 결정
      * 기본값은 모두 true

--- backend/src/main/java/com/app/backend/global/module/CustomPageModule.java
+++ backend/src/main/java/com/app/backend/global/module/CustomPageModule.java
@@ -1,5 +1,6 @@
 package com.app.backend.global.module;
 
+import com.app.backend.global.annotation.CustomPageJsonSerializer;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
@@ -9,32 +10,48 @@ import org.springframework.data.domain.Page;
 
 public class CustomPageModule extends SimpleModule {
 
-    public CustomPageModule() {
-        addSerializer(Page.class, new PageSerializer());
+    public CustomPageModule(CustomPageJsonSerializer annotation) {
+        addSerializer(Page.class, new PageSerializer(annotation));
     }
 
     private static class PageSerializer extends StdSerializer<Page> {
 
-        private PageSerializer() {
+        private final CustomPageJsonSerializer annotation;
+
+        private PageSerializer(CustomPageJsonSerializer annotation) {
             super(Page.class);
+            this.annotation = annotation;
         }
 
         @Override
         public void serialize(Page value, JsonGenerator gen, SerializerProvider provider) throws IOException {
             gen.writeStartObject();
-            gen.writeObjectField("content", value.getContent());
-            gen.writeBooleanField("hasContent", value.hasContent());
-            gen.writeNumberField("totalPages", value.getTotalPages());
-            gen.writeNumberField("totalElements", value.getTotalElements());
-            gen.writeNumberField("numberOfElements", value.getNumberOfElements());
-            gen.writeNumberField("size", value.getSize());
-            gen.writeNumberField("number", value.getNumber());
-            gen.writeBooleanField("hasPrevious", value.hasPrevious());
-            gen.writeBooleanField("hasNext", value.hasNext());
-            gen.writeBooleanField("isFirst", value.isFirst());
-            gen.writeBooleanField("isLast", value.isLast());
-            gen.writeObjectField("sort", value.getSort());
-            gen.writeBooleanField("empty", value.isEmpty());
+            if (annotation.content())
+                gen.writeObjectField("content", value.getContent());
+            if (annotation.hasContent())
+                gen.writeBooleanField("hasContent", value.hasContent());
+            if (annotation.totalPages())
+                gen.writeNumberField("totalPages", value.getTotalPages());
+            if (annotation.totalElements())
+                gen.writeNumberField("totalElements", value.getTotalElements());
+            if (annotation.numberOfElements())
+                gen.writeNumberField("numberOfElements", value.getNumberOfElements());
+            if (annotation.size())
+                gen.writeNumberField("size", value.getSize());
+            if (annotation.number())
+                gen.writeNumberField("number", value.getNumber());
+            if (annotation.hasPrevious())
+                gen.writeBooleanField("hasPrevious", value.hasPrevious());
+            if (annotation.hasNext())
+                gen.writeBooleanField("hasNext", value.hasNext());
+            if (annotation.isFirst())
+                gen.writeBooleanField("isFirst", value.isFirst());
+            if (annotation.isLast())
+                gen.writeBooleanField("isLast", value.isLast());
+            if (annotation.sort())
+                gen.writeObjectField("sort", value.getSort());
+            if (annotation.empty())
+                gen.writeBooleanField("empty", value.isEmpty());
             gen.writeEndObject();
         }
 
