9168fc1915627e865bd1c4fd976926114c3cd792
vdvhk12 <vdvhk12@gmail.com>
2025-02-26 20:45:52 +0900

    chore: 모니터링을 위한 의존성 추가 (#211)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -66,6 +66,10 @@ dependencies {
     //Querydsl
     annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
 
+    // monitoring
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.mockito:mockito-inline:5.2.0")
