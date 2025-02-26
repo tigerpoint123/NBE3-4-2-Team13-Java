111e4c09ab6b02194c2dbc67923f7b6e55914f91
GAEUN220 <gaeun2701@gmail.com>
2025-02-26 12:56:57 +0900

    fix: fix

--- backend/src/main/java/com/app/backend/domain/group/entity/GroupLike.java
+++ backend/src/main/java/com/app/backend/domain/group/entity/GroupLike.java
@@ -30,11 +30,11 @@ public class GroupLike extends BaseEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "group_id")
+    @JoinColumn(name = "group_id", nullable = false)
     private Group group;
 
     @Builder
