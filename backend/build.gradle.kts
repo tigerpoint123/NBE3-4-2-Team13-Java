872d0c6c607f7c9f4d248a4777235d2b0ec95bc1
vdvhk12 <vdvhk12@gmail.com>
2025-02-28 16:16:52 +0900

    chore: RabbitMQ 의존성 추가 (#211)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -70,6 +70,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus")
 
+    // RabbitMQ
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.mockito:mockito-inline:5.2.0")
