482ac3c6a0c65c2a6257764970e1c80b9c411519
Honam Kim <tigerrla@naver.com>
2025-05-24 16:52:25 +0900

    myInfo 정보 수정

--- frontend/src/app/member/info/ClientPage.tsx
+++ frontend/src/app/member/info/ClientPage.tsx
@@ -106,29 +106,29 @@ export default function ClientPage() {
   }, []);
   return (
     <div className='container mx-auto p-4 max-w-2xl'>
-      <div className='bg-white rounded-lg shadow-lg p-6 mb-6'>
+      <div className='bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 mb-6'>
         <div className='mb-6'>
-          <h2 className='text-2xl font-bold'>회원 정보</h2>
+          <h2 className='text-2xl font-bold dark:text-white'>회원 정보</h2>
         </div>
         <div className='space-y-4'>
           <div className='grid grid-cols-3 gap-4'>
-            <div className='font-semibold'>닉네임</div>
-            <div className='col-span-2'>{loginMember.nickname}</div>
+            <div className='font-semibold dark:text-gray-200'>닉네임</div>
+            <div className='col-span-2 dark:text-gray-300'>{loginMember.nickname}</div>
 
-            <div className='font-semibold'>회원 ID</div>
-            <div className='col-span-2'>{loginMember.username}</div>
+            {/* <div className='font-semibold'>회원 ID</div>
+            <div className='col-span-2'>{loginMember.username}</div> */}
 
-            <div className='font-semibold'>비밀번호</div>
-            <div className='col-span-2'>{loginMember.password}</div>
+            {/* <div className='font-semibold'>비밀번호</div>
+            <div className='col-span-2'>{loginMember.password}</div> */}
 
-            <div className='font-semibold'>가입일</div>
-            <div className='col-span-2'>{formatDate(loginMember.createdAt)}</div>
+            <div className='font-semibold dark:text-gray-200'>가입일</div>
+            <div className='col-span-2 dark:text-gray-300'>{formatDate(loginMember.createdAt)}</div>
 
             {/* <div className="font-semibold">회원 권한</div>
                         <div className="col-span-2">{formatAuthorities(loginMember.role.map(auth => ({authority: auth})))}</div> */}
 
-            <div className='font-semibold'>가입 방식</div>
-            <div className='col-span-2'>{loginMember.provider}</div>
+            {/* <div className='font-semibold'>가입 방식</div>
+            <div className='col-span-2'>{loginMember.provider}</div> */}
           </div>
         </div>
 
